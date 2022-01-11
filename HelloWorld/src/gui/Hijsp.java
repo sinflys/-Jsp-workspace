9cb52ea6ee7713d9266cdb56bd7d905a53724a76
ilho <sinflys@naver.com>
2022-01-11 16:30:56 +0900

    first commit1

--- HelloWorld/src/gui/Hijsp.java
+++ HelloWorld/src/gui/Hijsp.java
@@ -1,25 +1,34 @@
 package gui;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//HttpServlet �� ����� Ŭ���� => ����
+@WebServlet("/hi")
 public class Hijsp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		// get은 url 주소 요청시
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<b>HI!!!</b>");		
+		out.println("</html>");
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// form 요청시 예) 로그인 (주소창에 표시되지 않음)
 		super.doPost(req, resp);
 	}
+	
+	
 
 }
